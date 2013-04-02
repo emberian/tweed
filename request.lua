d5f7301d3bd74058348dd2b63f0f78f5f3c3aa85
Corey Richardson <corey@octayn.net>
2013-04-02 19:50:35 -0400

    Lazily fetch request body

--- request.lua
+++ request.lua
@@ -4,5 +4,13 @@ return function(env)
 	local request = {}
 	request.env = env
 	request.method = env.REQUEST_METHOD
+	setmetatable(request, {__index = function(tab, key)
+		local rawk = rawget(tab, key)
+		if rawk then return rawk end
+		if key == 'body' then
+			tab[key] = env.input:read()
+			return rawget(tab, key)
+		end
+	end})
 	return request
 end
